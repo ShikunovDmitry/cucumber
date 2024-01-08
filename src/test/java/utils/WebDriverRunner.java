ced0f06073cb1f7eb4d84cc03eb492ce78687f2e
ShikunovDmitry <140311418+ShikunovDmitry@users.noreply.github.com>
2024-01-08 21:21:45 +0400

    Update WebDriverRunner.java

--- src/test/java/utils/WebDriverRunner.java
+++ src/test/java/utils/WebDriverRunner.java
@@ -2,6 +2,13 @@ package utils;
 
 import org.openqa.selenium.WebDriver;
 import org.openqa.selenium.chrome.ChromeDriver;
+import org.openqa.selenium.chrome.ChromeOptions;
+import org.openqa.selenium.firefox.FirefoxOptions;
+import org.openqa.selenium.remote.DesiredCapabilities;
+import org.openqa.selenium.remote.RemoteWebDriver;
+
+import java.net.MalformedURLException;
+import java.net.URL;
 
 public class WebDriverRunner {
     private static WebDriver driver;
