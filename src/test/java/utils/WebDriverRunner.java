5367959d9c537a02f69e72c9725bf3edff369852
ShikunovDmitry <140311418+ShikunovDmitry@users.noreply.github.com>
2024-01-08 21:02:00 +0400

    Update WebDriverRunner.java

--- src/test/java/utils/WebDriverRunner.java
+++ src/test/java/utils/WebDriverRunner.java
@@ -6,8 +6,10 @@ import org.openqa.selenium.chrome.ChromeDriver;
 public class WebDriverRunner {
     private static WebDriver driver;
 
-    public static void initDriver() {
-        driver = new ChromeDriver();
+   private static final String serverURL = "http://192.168.100.18:4444/wd/hub";
+
+    public static void initDriver() throws MalformedURLException {
+        driver = new RemoteWebDriver(new URL(serverURL), new FirefoxOptions());
     }
 
     public static WebDriver getDriver() {
